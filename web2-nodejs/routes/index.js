6fc424c1225767f434b34bfaab1fb76660add2a0
kimtrasche <kimtrasche@outlook.com>
2019-05-01 23:45:05 +0900

    MySql 공부

--- web2-nodejs/routes/index.js
+++ web2-nodejs/routes/index.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var template = require('../lib/template.js')
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host     : '192.168.56.1',
+    user     : 'kt4u',
+    password : 'kt4u0512',
+    database : 'opentutorials'
+});
+db.connect();
 
-router.get('/', function (request, response) {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js'; 
-    var list = template.list(request.list);      
-    var html = template.html(title, list,
-            `<h2>${title}</h2>${description}
-            <img src="/images/photo.jpg" style="width:300px;display:block;">`, `<a href="/topic/create">create</a>`);
-    response.send(html);
+router.get('/', function (request, response) {    
+    db.query(`SELECT * FROM topic`, function(error, topics){        
+        var title = 'Welcome';
+        var description = 'Hello, Node.js'; 
+        var list = template.list(topics);      
+        var html = template.html(title, list,
+                `<h2>${title}</h2>${description}
+                <img src="/images/photo.jpg" style="width:300px;display:block;">`,
+                 `<a href="/topic/create">create</a>`);
+        response.send(html);
+    });
 });
 
 module.exports=router;
--- web2-nodejs/routes/topic.js
+++ web2-nodejs/routes/topic.js
@@ -4,107 +4,174 @@ var sanitizeHtml = require('sanitize-html')
 var path = require('path');
 var template = require('../lib/template.js')
 var fs = require('fs');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host     : '192.168.56.1',
+    user     : 'kt4u',
+    password : 'kt4u0512',
+    database : 'opentutorials'
+});
+db.connect();
+
+
 
 router.get('/create', function (request, response){
-    var title = 'WEB-create';            
-    var list = template.list(request.list);      
-    var html = template.html(title, list, 
-        `
-        <form action="/topic/create_process" method="POST"> 
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-            <textarea name="description" placeholder="descrion"></textarea>
-        </p>
-        <p>
-            <input type="submit">
-        </p>
-        </form>
-        `
-        ,``
-    );
-    response.send(html);
+    db.query(`SELECT * FROM topic`, function(error, topics){
+        db.query('SELECT * FROM author', function(error2, authors){
+            var title = 'WEB-create';            
+            var list = template.list(topics);      
+            var html = template.html(title, list,
+                `
+                <form action="/topic/create_process" method="POST"> 
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <select name="author">
+                        ${template.authorSelect(authors)}
+                    </select>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+                </form>
+                `
+                ,``    
+            );
+            response.send(html);
+        });                
+    });    
 });
 
 router.post('/create_process', function(request, response){
     var post = request.body;
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`,description,'utf8', function(err){            
-        response.redirect(`/topic/${title}`);
-    });            
+    db.query(`
+        INSERT INTO topic (title, description, created, author_id) 
+            VALUES (?, ?, NOW(), ?)`,
+        [post.title, post.description, post.author],
+        function(error, result){
+            if(error){
+                throw error;
+            }else{
+                response.redirect(`/topic/${result.insertId}`);
+            }
+        }
+    );
 });
-
-router.get('/:pageId', function (request, response, next) {
-    var list = template.list(request.list);
-    var filteredId = path.parse(request.params.pageId).base;//보안
-    fs.readFile(`data/${filteredId}`,'utf8', function(err,description){
-        if(err){
-            next(err);
-        }else{
-            var title = request.params.pageId;
-            var sanitizedTitle= sanitizeHtml(title);
-            var sanitizedDesc = sanitizeHtml(description);
-            var html = template.html(sanitizedTitle, list,
-                `<h2>${sanitizedTitle}</h2><p>${sanitizedDesc}</p>`
-                , `<a href="/topic/create">create</a> 
-                    <a href="/topic/update/${title}">update</a> 
-                    <form action="/topic/delete_process" method="post">
-                    <input type="hidden" name="id" value="${title}">
-                    <input type="submit" value="delete">
-                    </form>                           
-                    `);
-            response.send(html);
-        }        
-    });
-});
-
-
 
 
 router.get('/update/:pageId', function(request,response){
-    var list = template.list(request.list);
-    var filteredId = path.parse(request.params.pageId).base;//보안
-    fs.readFile(`data/${filteredId}`,'utf8', function(err,description){
-        var title = request.params.pageId;                    
-        var html = template.html(title, list, 
-            `
-            <form action="/topic/update_process" method="POST"> 
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-            <p>
-                <textarea name="description" placeholder="descrion">${description}</textarea>
-            </p>
-            <p>
-                <input type="submit">
-            </p>
-            </form>
-            `
-            , `<a href="/topic/create">create</a> <a href="/update/${title}">update</a>`
-        );
-        response.send(html);
-    });
+    db.query('SELECT * FROM topic', function(error, topics){
+        if(error){
+            throw error;
+        }
+        db.query('SELECT * FROM topic WHERE id=?',[request.params.pageId], function(error2, topic){
+            if(error2){
+                throw error2;
+            }
+            db.query('SELECT * FROM author', function(error3, authors){
+                var list = template.list(topics);
+                var title = topic[0].title;
+                var description = topic[0].description;
+                var html = template.html(title, list, 
+                    `
+                    <form action="/topic/update_process" method="POST"> 
+                    <input type="hidden" name="id" value="${request.params.pageId}">
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p>
+                        <textarea name="description" placeholder="description">${description}</textarea>
+                    </p>
+                    <p>
+                        <select name="author">
+                            ${template.authorSelect(authors, topic[0].author_id)}
+                        </select>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                    </form>
+                    `
+                    , `<a href="/topic/create">create</a> <a href="/update/${topic[0].id}">update</a>`
+                );
+                response.send(html);            
+            });            
+        });
+    });    
 });
 
 router.post('/update_process',function(request, response){
     var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`,description,'utf8', function(err){    
-            response.redirect(`/topic/${title}`);     
-        });
-    });
-
+    db.query(`
+    UPDATE topic 
+       SET TITLE = ?
+         , DESCRIPTION = ?
+         , AUTHOR_ID = ?
+     WHERE ID = ?`, 
+        [post.title, post.description, post.author, post.id],
+        function(error, result){
+            if(error){
+                throw error;
+            }else{
+                response.redirect(`/topic/${post.id}`);
+            }
+        }
+    );
 });
 
+
 router.post('/delete_process', function(request, response){
+    console.log('delete');
     var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;//보안
-    fs.unlink(`data/${filteredId}`, function(err){
+    db.query('DELETE FROM topic WHERE ID = ?',[post.id], function(error, result){
+        if(error){
+            throw error;
+        }
         response.redirect('/');
     });
 });
+
+
+router.get('/:pageId', function (request, response, next) {
+    db.query(`SELECT * FROM topic`, function(error, topics){ 
+        if(error){
+            throw error;
+        }
+        db.query(`SELECT t.id AS id 
+                       , t.title
+                       , t.description
+                       , a.name
+                  FROM topic t
+                  LEFT JOIN author a
+                    ON t.author_id = a.id
+                 WHERE t.id=?`,request.params.pageId, function (error2, topic){
+            if(error2){
+                throw error2;
+            }
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);      
+            var sanitizedTitle= sanitizeHtml(title);
+            var sanitizedDesc = sanitizeHtml(description);
+            var html = template.html(sanitizedTitle, list,
+                `<h2>${sanitizedTitle}</h2>
+                 <p>${sanitizedDesc}</p> by ${topic[0].name}
+                `
+                , `<a href="/topic/create">create</a> 
+                    <a href="/topic/update/${topic[0].id}">update</a> 
+                    <form action="/topic/delete_process" method="post">
+                    <input type="hidden" name="id" value="${topic[0].id}">
+                    <input type="submit" value="delete">
+                    </form>                           
+                    `);
+            response.send(html);
+        });        
+    });
+});
+
+
+
+
+
 
 module.exports = router;
